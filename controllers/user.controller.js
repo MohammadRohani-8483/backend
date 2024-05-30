8f7043c0ed34a243ae4ad6b02f15b58b08b95b80
MohammadRohani-8483 <mohammad.rohani.1002@gmail.com>
2024-05-30 23:59:30 +0330

    Delete additional log

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,10 +5,8 @@ export const getUsers = async (req, res) => {
     const logedInUserid = req.user._id;
 
     const filteredUsers = await User.find({ _id: { $ne: logedInUserid } });
-    console.log(filteredUsers);
 
     res.status(200).json(filteredUsers);
-    // res.status(200).json({ message: "hello" });
   } catch (err) {
     console.error("Error in get users controller: ", err.message);
     res.status(500).json({ error: "خطای سرور داخلی رخ داده است!" });
